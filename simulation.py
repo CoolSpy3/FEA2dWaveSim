5492e06c06294f8e7fe9c8669e0243bed5bb4110
CoolSpy3 <coolspythree@gmail.com>
2026-04-26 13:15:30 -0700

    a more accurate test case

--- simulation.py
+++ simulation.py
@@ -14,7 +14,8 @@ def frame(n:int):
 
 	for x in range(100):
 		for y in range(100):
-			grid[x][y] = np.sin(np.sqrt((x**2 + y**2)) + n)
+			r = np.sqrt(x**2 + (y - 50)**2)
+			grid[y][x] = np.sin(r - n) / np.sqrt(r)
 
 	map_data.set_data(grid)
 
